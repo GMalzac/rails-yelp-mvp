0ce2c0d2f1284a0cfc02b10be15d437ce47dcc67
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-02-21 14:10:39 +0000

    ex 1 done, rake OK, no links and no CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.new(name:"Bristol", address:"Paris", phone_number: "+3366666666", category: "french").save!
+Restaurant.new(name:"Bones", address:"London", phone_number: "+4476456789", category: "chinese").save!
+Restaurant.new(name:"Mc Do", address:"Lisboa", phone_number: "+3878897666666", category: "italian").save!
+Restaurant.new(name:"Dishoom", address:"London", phone_number: "+443456789098765", category: "japanese").save!
+Restaurant.new(name:"Chicks", address:"London", phone_number: "+4445666666666", category: "belgian").save!
